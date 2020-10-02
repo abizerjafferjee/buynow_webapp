2875cad3b4a3cb423dc1e45fa8be7b7fab7996cb
abizerjafferjee <abizerjafferjee@gmail.com>
2020-10-02 13:25:08 -0400

    config

--- src/constants/Axios.js
+++ src/constants/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const serverUrl = 'http://localhost:8000'
-// const serverUrl = 'https://api.odiance.com'
+// export const serverUrl = 'http://localhost:8000'
+const serverUrl = 'https://api.odiance.com'
 
 export const axiosInstance = axios.create({
     withCredentials: true,
