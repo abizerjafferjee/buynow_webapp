8910a04f33fea1445071b3eeb114abfab9b97981
abizerjafferjee <abizerjafferjee@gmail.com>
2020-10-06 20:04:41 -0400

    fix

--- src/constants/Axios.js
+++ src/constants/Axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
 // export const serverUrl = 'http://localhost:8000'
-const serverUrl = 'https://api.odiance.com'
+export const serverUrl = 'https://api.odiance.com'
 
 export const axiosInstance = axios.create({
     withCredentials: true,
     baseURL: serverUrl
 })
-
-
